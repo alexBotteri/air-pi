17bda66b2aa208201311604ad829ec2798273761
alex <alex.botteri@gmail.com>
2020-09-23 15:18:10 -0700

    mapping and display Conerns and AQI score

--- air-pi.py
+++ air-pi.py
@@ -2,17 +2,37 @@ import Pms5003Controller
 import Display1306Controller
 from time import sleep
 
-ser = Pms5003Controller.configSerialPort()
-Pms5003Controller.setPMSSensorInPassiveMode(ser)
-disp = Display1306Controller.setupDisplay()
+def main(): 
+    ser = Pms5003Controller.configSerialPort()
+    Pms5003Controller.setPMSSensorInPassiveMode(ser)
+    disp = Display1306Controller.setupDisplay()
 
-while True:
-    sleep(5)
-    aqiDisplayLine = "AQI: " + Pms5003Controller.readDataFromPMSSensor(ser)['aqi']
-    lines = [
-        "AIR-PI", 
-        aqiDisplayLine,
-        "-------------------", 
-        "", 
-        ""]
-    Display1306Controller.displayLines(disp, lines)
+    while True:
+        sleep(5)
+        aqi = Pms5003Controller.readDataFromPMSSensor(ser)['aqi']    
+        aqiConcernDisplayLine = aqiScoreConcern(aqi)
+        aqiDisplayLine = "AQI: " + aqi
+        lines = [
+            aqiConcernDisplayLine, 
+            aqiDisplayLine,
+            "-------------------", 
+            "by AIR-PI", 
+            ""]
+        Display1306Controller.displayLines(disp, lines)
+
+
+
+def aqiScoreConcern(aqi):
+    concernMapping = {
+        '50': 'Good', 
+        '100': 'Moderate',
+        '150': 'Unhealthy for SG',
+        '200': 'Unhealthy',
+        '300': 'Very unhealthy',
+        '999999': 'Hazardous'}
+
+    for score in concernMapping:
+        if int(aqi) < int(score):
+            return concernMapping[score]
+
+main()
